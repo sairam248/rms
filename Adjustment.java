1cff88646135f1c06a65bc84e246cf072cd9937e
chagantisairam <chagantisairam98@gmial.com>
2017-09-14 18:42:09 +0530

    Update Adjustment.java

--- Adjustment.java
+++ Adjustment.java
@@ -11,14 +11,14 @@ public class Adjustment implements Serializable{
 	private String description;
 	private Double amount = 0.0;
 	private Integer id_booking;
-	
+	@override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
+	@override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
