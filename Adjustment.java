e845668414e53c187b9bbf576806fe9e3f47c232
chagantisairam <chagantisairam98@gmial.com>
2017-09-14 17:42:23 +0530

    Update Adjustment.java

--- Adjustment.java
+++ Adjustment.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- *
- *  Copyright 2011 - Sardegna Ricerche, Distretto ICT, Pula, Italy
- *
- * Licensed under the EUPL, Version 1.1.
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at:
- *
- *  http://www.osor.eu/eupl
- *
- * Unless required by applicable law or agreed to in  writing, software distributed under the Licence is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and limitations under the Licence.
- * In case of controversy the competent court is the Court of Cagliari (Italy).
- *******************************************************************************/
 package model;
 
 import java.io.Serializable;
@@ -27,15 +12,13 @@ public class Adjustment implements Serializable{
 	private Double amount = 0.0;
 	private Integer id_booking;
 	
-		
-	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	@Override
+	
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
